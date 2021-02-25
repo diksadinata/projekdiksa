f29a21a721e345a365383e96b2b6863fde827e94
diksadinata <diksadinata@gmail.com>
2021-02-25 22:09:05 +0800

    projek pertama

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE  html>
+<html  lang="en">
+<head>
+    <title>animekun</title>
+    <meta  charset="utf-8">
+    <meta  name="viewport"  content="width=device-width,  initial-scale=1">
+<link  rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstr ap.min.css">
+    <script  src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link  rel="stylesheet"type="text/css"href="style.css">
+</head>
+<nav>
+        <a href="index.html">Home</a> |
+        <a href="cv-dian.pdf">Download</a> |
+        <a href="contact.html">Contact</a> |
+        <a href="about.html">About me</a>|
+        <a href="login.html">Login</a> |
+    </nav>
+<header>
+    <div  class="container  ">
+        <div  class="navbar"  style="background-image:  url('thumb-1920-545905.jpg');  margin:  0px;">
+            <div  class="pull-left">
+            <img src="media/thumb-1920-5459051.jpg" width="100" height="100" style="border-radius: 50%;" />
+                </div>
+                <div  class="pull-left">
+                        <h2>Selamat  Datang</h2>
+                        <h8>creator:diksa dinata</h8>
+                </div>
+            </div>
+</header>
+<body>
+            <article style="text-align: center;">
+             <h1>ANIME 2021</h1>
+             <p>
+            2021 merupakan tahun yang sangat membahagiakan bagi para penikmat anime, karna tahun ini sangat banyak sekali anime yang di tunggu-tunggu akhirnya di rilis di tahun 2021
+            <P>
+            Tidak terasa musim gugur berjalan dengan cepat dan kita telah mulai memasuki musim dingin. Itu artinya akan ada banyak anime baru yang menanti untuk kita tonton. Pada artikel kali ini, IDN Times Community akan berbagi judul-judul anime menarik untuk kita tonton di musim dingin 2021 ini. Apa sajakah judulnya? Yuk, kita simak bersama-sama! 
+<P>
+1. Dr. STONE: STONE WARS
+<P>
+<img src="media/981906.jpg" width="500" height="320"  />
+<p>
+Season kedua dari petualangan Senku, sang scientist, bersama teman-temannya ini akan yang berusaha mengembalikan dunia kembali seperti semula setelah kejadian aneh yang membuat seluruh manusia di muka bumi menjadi batu. Seperti judulnya, yaitu Stone Wars, season kali ini berfokus pada pertempuran melawan Tsukasa Shisio dan tentunya mengandalkan ilmu pengetahuan yang luar biasa dari Senku.<p>
+Season kedua dari anime yang merupakan adaptasi dari manga berjudul sama ini akan kembali dikerjakan oleh TMS Entertainment dan rencananya akan rilis pada 14 Januari 2021 mendatang. Untuk kamu yang sudah menonton season pertama, pasti sudah tidak sabar lagi melihat sepak terjang Senku, bukan? <P>
+2. Yakusoku no Neverland season 2
+<P>
+<img src="media/1001742.png" width="500" height="320"  />
+<p>
+Setelah akhirnya berhasil kabur dari tempat dia dibesarkan, Emma dan kawan-kawan dihadapkan dengan tantangan baru yaitu bertahan hidup di dunia yang sama sekali asing dengan mereka. Beruntung, mereka bertemu dengan dua orang rekan yang tak disangka sebelumnya. Lambat laun, mereka semakin mampu untuk bertahan hidup. <p>
+
+Season kedua dari anime ini merupakan kelanjutan dari season pertama saat Emma pertama kali mengetahui rahasia sebenarnya dari tempat dia dibesarkan. Hal itu mendorong Emma untuk kabur dari tempat tersebut sambil membawa saudara-saudaranya yang masih kecil. Namun, apakah mereka bisa bertahan hidup di luar sana? Anime ini mulai tayang pada 7 Januari 2021.<p>
+3. Go-toubun no Hanayome ∬
+<p>
+<img src="media/1002864.png" width="500" height="320"  />
+<p>
+Season kedua dari Waifu War antara lima saudari kembar untuk memperebutkan Futaro Uesugi yang merupakan guru privat mereka. Setelah season pertama anime ini tayang, semakin banyak yang dibuat penasaran siapakah yang akhirnya akan dipinang oleh Futaro Uesugi dan saling berdebat siapakah yang terbaik dari kelima saudari tersebut.<p>
+
+Sebagai informasi, pada awal kisah anime ini, salah satu dari kelima saudari tersebut akan menikah dengan Futaro Uesugi. Namun, karena mereka kembar, sulit untuk mengetahui siapakah orang tersebut. Untuk kamu yang juga dibuat penasaran, anime ini bisa kamu saksikan pada 7 Januari 2021 mendatang, lho.
+</P>
+             </p>
+            </article>
+
+            <div  class="post-info">
+        Di  Posting  Oleh  <b>Admin</b>
+</div>
+    <table border="1" width="100%">
+        <thead>
+            <tr>
+                <th>daftar Anime</th>
+                <th>Tanggal rilis</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <ul>
+                        <li>Dr. STONE: STONE WARS</li>
+                        <li>Yakusoku no Neverland season 2</li>
+                        <li> Go-toubun no Hanayome ∬</li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li>1   JANUARI   2021</li>
+                        <li>2   FEBRUARI  2021</li>
+                        <li>17  JANUARI   2021</li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+
+            <a  href="#"  class="continue-lendo">Selengkapnya  →</a>
+        </div>
+    <hr>
+</div>
+            <div  class="col-lg-4  "  style="background-color:  black  ">
+        <div  class="widget  ">
+    <h2>Info anime popular </h2>
+    <ul  class="menusidebar  ">
+<li><a  href="  ">Attak on titan tayang 2021</a></li>
+<li><a  href="  ">kabar naruto mati</a></li>
+</ul>
+</div>
+<div  class="widget">
+<h2>Kumpulan Wallpaper keren</h2>
+<ul  class="menusidebar">
+                <li><a  href="">Wallpaper anime 2021</a></li>
+                <li><a  href="">Wallpaper alam</a></li>
+                <li><a  href="">Walpaper animal</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+</div>
+<div  id="myModal  "  class="modal  fade  "  role="dialog  ">
+<div  class="modal-dialog  ">
+<!--  Modal  content-->
+<div  class="modal-content  ">
+<div  class="modal-header  ">
+<button  type="button  "  class="close  "  data- dismiss="modal  ">&times;</button>
+<h4  class="modal-title  ">Berhasil</h4>
+</div>
+<div  class="modal-body  ">
+<p></p>
+</div>
+<div  class="modal-footer  ">
+<button  type="button  "  class="btn  btn-default  "  data- dismiss="modal  ">Close</button>
+<hr>
+        <footer style="text-align: center;">
+            <p>Copyright &copy; 2021 animekun.com.</p>
+        </footer>
+            
+        </body>
+</body>
+
+</html>
+
+</div>
+</div>
+
+</div>
+</div>
+</body>
+
+</html>
